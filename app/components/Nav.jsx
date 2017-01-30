d924aee118a4a7ab99aa92be262579c38dc2ba4c
karim <karim.liaquat@gmail.com>
2017-01-31 00:57:45 +0500

    Nav bar style added

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,27 @@
 var React = require('react')
-var {Link,IndexLink} = require('react-router')
+var {Link, IndexLink} = require('react-router')
 var Nav = React.createClass({
-  render : function(){
-    return (
-      <div>
-      <h3>Nav bar loaded</h3>
-      <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-      <IndexLink to ="/about" activeClassName="active">About</IndexLink>
-      <IndexLink to ="/examples" activeClassName="active">Example</IndexLink>
-    </div>
-    );
-  }
+    render: function() {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                      <li class="menu-text">React Application</li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <IndexLink to="/about" activeClassName="active">About</IndexLink>
+                        </li>
+                        <li>
+                            <IndexLink to="/examples" activeClassName="active">Example</IndexLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right"></div>
+            </div>
+        );
+    }
 })
 
-module.exports = Nav
+module.exports = Nav;
